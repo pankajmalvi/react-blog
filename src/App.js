d107b44c5d92a6691d3468e7a823d0bb7d9b7674
pankajmalvi <pankaj.17jics105@jietjodhpur.ac.in>
2022-10-21 11:44:45 +0530

    Added Possible Routes and Basic Functionality of the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NewBlog from './pages/NewBlog';
+import Blog from './components/Blog';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/new">New Post</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/new' element={<NewBlog />} />
+        <Route path='/blog/:id' element={<Blog />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/NewBlog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NewBlog() {
+    return (
+        <div>NewBlog</div>
+    )
+}
+
+export default NewBlog
